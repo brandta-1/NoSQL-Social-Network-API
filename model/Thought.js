bc3828af57b223e5efe5a51f06b933961a849eda
Alex Brandt <alexezemail@gmail.com>
2023-06-03 16:12:00 -0400

    init and user model

---
+++ model/Thought.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true,
+        }
+    }
+)
